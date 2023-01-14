c0bbf466abe45941fcf0daf6253345594be528cb
JarneSimons <r0901961@student.thomasmore.be>
2023-01-15 00:17:47 +0100

    adding new screen content

--- screens/playSongs.js
+++ screens/playSongs.js
@@ -1,13 +1,47 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, FlatList, Image, TextInput, Pressable, TouchableNativeFeedback, TextComponent, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, FlatList, Image, TextInput, Pressable, TouchableNativeFeedback, TextComponent, TouchableOpacity, ActivityIndicator} from 'react-native';
 
 
-const PlaySongs = () => {
+const PlaySongs = (route, data) => {
     return(
-        <View>
-            <Text>het werkt</Text>
+        <View style={styles.boxIndicator}>
+            <ActivityIndicator style={styles.loadingStyle} size={"large"} color="#39FF13"/>
+
+            
+            <View>
+                <Text style={styles.textStyling}>
+                    Oops something went wrong! Please go back to the previous page.
+                </Text>
+            </View>
+
+
+
         </View>
+
+        
     )
+
 };
 
 export default PlaySongs;
+
+const styles = StyleSheet.create ({
+    boxIndicator:{
+        backgroundColor: "#010B12",
+        height: "100%",
+        alignItems: 'center',
+        
+        
+    }, 
+    loadingStyle: {
+        paddingTop: 150,
+        
+    }, textStyling: {
+        color: "#f5f5f5",
+        fontSize: 24,
+        paddingLeft: 15,
+        paddingRight: 15,
+        marginTop: 50
+    }
+
+})
