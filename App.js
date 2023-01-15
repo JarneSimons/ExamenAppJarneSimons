8dd137f33723c71aa0098738b748cf586444be18
JarneSimons <r0901961@student.thomasmore.be>
2023-01-15 12:35:02 +0100

    finishing play song page

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import SongsDetail from './screens/songsDetail.js';
 import Songs from './screens/songs.js';
-import Reactions from './screens/reactions.js'; 
+import Reactions from './screens/reactions.js';
+import PlaySongs from './screens/playSongs.js';
 
 const Stack = createNativeStackNavigator();//
 
@@ -21,6 +22,7 @@ export default function App() {
         <Stack.Screen name ="Home" component={Songs} />
         <Stack.Screen name ="songsDetail" component={SongsDetail} />
         <Stack.Screen name ="reactions" component={Reactions}/>
+        <Stack.Screen name = "playSongs" component={PlaySongs}/>
       </Stack.Navigator>
    </NavigationContainer>
   );
